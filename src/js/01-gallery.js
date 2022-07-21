cdd057f13a9f81d94af18caee0804897db14fe7f
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-07-21 23:51:26 +0300

    gallery add simpleLightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// let gallery = new SimpleLightbox('.gallery a');
+
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery')
+const galleryMarkup = createGalleryMarkup(galleryItems)
+
+galleryEl.innerHTML = galleryMarkup
+
+let gallery = new SimpleLightbox('.gallery a', {
+	captions: true,
+	captionSelector: 'img',
+	captionType: 'attr',
+	captionsData: 'alt',
+	captionPosition: 'bottom',
+	captionDelay: 250,
+	});
+
+function createGalleryMarkup (gallery){
+return gallery.map(({preview, original, description}) => 
+`
+<li class="gallery__item">        
+<a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li> `).join('')
+}
+
